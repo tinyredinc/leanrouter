9b86477bf5c0dab1677d3b2ea08632a47c549c46
tinyredinc <samforreg@gmail.com>
2018-09-07 20:13:32 +0800

    refactor

--- www/router.config.php
+++ www/router.config.php
@@ -12,6 +12,7 @@ $router_config['__undefine__']	= array('__page__'=>'404.phtml');
 $router_config['home']			= array('__page__'=>'home.phtml');
 $router_config['element']		= array('__page__'=>'element.phtml');
 $router_config['generic']		= array('__page__'=>'generic.phtml');
+$router_config['echart']		= array('__page__'=>'echart.phtml');
 
 #$router_config['support']['multiple']['tier'] = array('__page__'=>'multile_tier.phtml');
 
